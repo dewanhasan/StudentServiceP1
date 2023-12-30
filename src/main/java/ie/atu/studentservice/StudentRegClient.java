f2891971a186ca85aeb462579b4fe732c56c8d6a
dewanhasan99 <g00410156@atu.ie>
2023-12-30 14:02:07 +0000

    Containerised using docker

--- src/main/java/ie/atu/studentservice/StudentRegClient.java
+++ src/main/java/ie/atu/studentservice/StudentRegClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
 
-@FeignClient(name = "reg-service", url = "http://localhost:8082")
+@FeignClient(name = "reg-service", url = "http://reg-service:8082")
 public interface StudentRegClient {
     @PostMapping("/approved")
     Map<String, String> someDetails(@RequestBody StudentDetails studentDetails);
